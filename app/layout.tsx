1f5b8900c65b9a8dde0e54ae014e40302c2badcc
Jana Khatab <jana123mkhatab@gmail.com>
2026-09-03 02:18:34 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <AccessibilityProvider>
           <AccessibilityStatusRail />
           {children}
